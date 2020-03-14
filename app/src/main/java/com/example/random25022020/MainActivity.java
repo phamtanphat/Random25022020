deca6168fda7ac8a8b06c6be0eacf91c833d30cd
phatdroid94@gmail.com <phatdroid94@gmail.com>
2020-03-14 21:34:20 +0700

    Math.random

--- app/src/main/java/com/example/random25022020/MainActivity.java
+++ app/src/main/java/com/example/random25022020/MainActivity.java
@@ -15,13 +15,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // Math
         // Lam tron
-        Double a = 1.99;
+//        Double a = 1.99;
 //        long value = Math.round(a);
         // Lam tron len
 //        double value1 = Math.ceil(a);
         //Lam tron xuong
 //        double value2 = Math.floor(a);
 //        Log.d("BBB",String.valueOf(value2));
+        // Can hai
+//        double b = 9;
+//        double value = Math.sqrt(b);
+//        Log.d("BBB",String.valueOf(value));
+        // Tinh so max va min
+//        int a  = 5;
+//        int b = 6;
+//        int value = Math.max(a,b);
+//        Log.d("BBB",String.valueOf(value));
+        double value = Math.floor(Math.random()  * 6);
+        Log.d("BBB",String.valueOf(value));
 
     }
 }
