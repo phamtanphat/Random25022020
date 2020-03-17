95e890b54ea3d80211f258c40a1a43b44bafc0e6
phatdroid94@gmail.com <phatdroid94@gmail.com>
2020-03-17 21:11:38 +0700

    ui app

--- app/src/main/java/com/example/random25022020/MainActivity.java
+++ app/src/main/java/com/example/random25022020/MainActivity.java
@@ -9,47 +9,10 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    int count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Math
-        // Lam tron
-//        Double a = 1.99;
-//        long value = Math.round(a);
-        // Lam tron len
-//        double value1 = Math.ceil(a);
-        //Lam tron xuong
-//        double value2 = Math.floor(a);
-//        Log.d("BBB",String.valueOf(value2));
-        // Can hai
-//        double b = 9;
-//        double value = Math.sqrt(b);
-//        Log.d("BBB",String.valueOf(value));
-        // Tinh so max va min
-//        int a  = 5;
-//        int b = 6;
-//        int value = Math.max(a,b);
-//        Log.d("BBB",String.valueOf(value));
-//        double value = Math.floor(Math.random()  * 6);
-//        Log.d("BBB",String.valueOf(value));
 
-        // 0 - 5
-//        Random random = new Random();
-//        for (int i = 0 ; i < 100 ; i++){
-//            int value = random.nextInt(7) - random.nextInt(7);
-//            if (value == 6 || value == -6){
-//                count++;
-//            }
-//        }
-
-//        for (int i = 0 ; i < 100 ; i++){
-//            double value = Math.ceil(Math.random() * (-7)) + Math.floor(Math.random() * (7));
-//            if (value == 6 || value == -6){
-//                count++;
-//            }
-//        }
-//        Log.d("BBB",count+"");
     }
 }
